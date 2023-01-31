ea970d5729bd89789277926c5ccc3186c2235d63
Aayushikumari17 <115334350+Aayushikumari17@users.noreply.github.com>
2023-01-31 22:57:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const inputDegree = document.querySelector("#Degree")
+const degreeType = document.querySelector("#Degree_type")
+const result = document.querySelector("#result")
+const time = document.querySelector(".time")
+
+
+
+inputDegree.addEventListener('keyup',(e)=>{
+    
+    if (isNaN(e.target.value)){
+        e.target.value = '';
+    }
+});
+
+
+const button = document.querySelector(".convert-btn")
+
+button.addEventListener('click', convert);
+
+function convert(){
+    let selectedElem = degreeType.value
+    console.log(selectedElem);
+    if (selectedElem === "Fahrenheit") {
+        result.value = (5*(parseInt(inputDegree.value) - 32)/9 ).toFixed(2) + ' C' ;
+    }
+    else if(selectedElem === 'Celcious'){
+            result.value = ((9 * parseInt(inputDegree.value))/ 5 +32).toFixed(2)  + " F"
+        }
+}
+
+const today = new Date()
+time.innerHTML = today.getHours() + ":" +[(today.getMinutes() < 10 ) ?  '0'+today.getMinutes(): today.getMinutes()]
+
